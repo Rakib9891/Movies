ed346f8e61ed497c824dbfaa617059a0077a49a2
Rakibul Hasan <rakibulhasantanvir0@gmail.com>
2025-07-06 23:56:01 +0600

    add movies thubnail again

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ function App(){
             < Like title={"Thor"} pic={"https://i.pinimg.com/originals/52/3f/2c/523f2cb401311665564bb4681938d98b.jpg"} />
             < Like title={"Captain America"} pic={"https://wallpapercave.com/wp/wp3775641.jpg"} />
             < Like title={"Mad Max: Fury Road"} pic={"https://wallpapercat.com/w/full/d/9/9/329676-1920x1080-desktop-full-hd-mad-max-fury-road-background.jpg"} />
-            < Like title={"Iron Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
-            < Like title={"Iron Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
-            < Like title={"Iron Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
-            < Like title={"Iron Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
-            < Like title={"Iron Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
-            < Like title={"Iron Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
+            < Like title={"Iron-Man 2"} pic={"https://wallpapercat.com/w/full/c/6/e/320077-2560x1440-desktop-hd-iron-man-wallpaper-image.jpg"} />
+            < Like title={"Bullet Train"} pic={"https://www.ecranlarge.com/content/uploads/2022/08/bullet-train-affiche-1442748-320x170.jpg"} />
+            < Like title={"Deadpool & Wolverine"} pic={"https://wallpapercat.com/w/full/6/0/c/5822914-2880x1620-desktop-hd-deadpool-wolverine-comics-background-image.jpg"} />
+            < Like title={"Iron-Man 3"} pic={"https://wallpaper.forfun.com/fetch/97/970ef0b7721e8a279b4b0d2f1fffb570.jpeg"} />
+            < Like title={"Spider-Man"} pic={"https://m.media-amazon.com/images/M/MV5BNzE5NTk5NDQ0NV5BMl5BanBnXkFtZTgwNjU3ODIxMjI@.jpg"} />
+            < Like title={"Iron-Man"} pic={"https://wallpapercave.com/wp/wp3198150.jpg"} />
   
       </div>
       </main>
